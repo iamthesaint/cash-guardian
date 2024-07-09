480ad755a7901639bb7184ab66f40f83d44c9211
iamthesaint <stephenie2@me.com>
2024-07-09 18:52:40 -0400

    css frameworks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
-var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-}
-ready(() => {
-    document.querySelector(".header").style.height = window.innerHeight + "px";
-})
+const data = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+
+const config = {
+    type: 'doughnut',
+    data: data,
+  };
